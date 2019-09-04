27639b9511501454d920c03acc66eb925ccffb66
Ryan Gabbard <gabbard@isi.edu>
2019-09-04 17:21:38 -0400

    Fix syntax error in generated __init__.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,2 +1,2 @@
 # pylint:disable=missing-docstring
-from {{cookiecutter.project_slug}}.version import version as __version__ . #noqa
+from {{cookiecutter.project_slug}}.version import version as __version__  #noqa
